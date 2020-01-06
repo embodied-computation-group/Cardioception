4ed5d4d4aec6592419eba78026aac8beab1ee538
Nicolas Marc Simon Legrand <au646069@uni.au.dk>
2020-01-06 12:20:37 +0100

    HRD: test() file

--- cardioception/HeartRateDiscrimination/test.py
+++ cardioception/HeartRateDiscrimination/test.py
@@ -5,20 +5,18 @@ from cardioception.HeartRateDiscrimination.task import run
 from psychopy import gui
 
 
-def test_hrd():
+# Create a GUI and store subject ID
+g = gui.Dlg()
+g.addField("Serial Port:")
+g.show()
 
-    # Create a GUI and store subject ID
-    g = gui.Dlg()
-    g.addField("Serial Port:")
-    g.show()
+# Set global task parameters here
+parameters = getParameters('test', 1, g.data[0])
+parameters['nTrials'] = 1
 
-    # Set global task parameters here
-    parameters = getParameters('test', 1, g.data[0])
-    parameters['nTrials'] = 1
+# Run task
+run(parameters, win=parameters['win'],
+    confidenceRating=True, runTutorial=True)
 
-    # Run task
-    run(parameters, win=parameters['win'],
-        confidenceRating=True, runTutorial=True)
-
-    # Save results
-    parameters['win'].close()
+# Save results
+parameters['win'].close()
