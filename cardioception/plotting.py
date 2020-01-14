e8a4de603a9e47823af6276618be097cb4b65452
micahgallen <42416756+micahgallen@users.noreply.github.com>
2020-01-14 09:38:26 +0100

    Minor changes to plotting function.

--- cardioception/plotting.py
+++ cardioception/plotting.py
@@ -184,11 +184,12 @@ def plot_confidence(confidence, accuracy, path=None):
     for conf in range(1, 8):
         # Correct trials
         p = sum((accuracy == 0) & (confidence == conf)) / sum(accuracy == 0)
-        plt.bar(conf-0.15, p, width=0.30, color='r')
+        plt.bar(conf-0.15, p, width=0.30, color='r', label = 'Error Trials')
 
         # Incorrect trials
         p = sum((accuracy == 1) & (confidence == conf)) / sum(accuracy == 1)
-        plt.bar(conf+0.15, p, width=0.30, color='g')
+        plt.bar(conf+0.15, p, width=0.30, color='g', label = 'Correct Trials')
+    plt.legend()
     plt.ylabel('P(Rating|Precision)')
     plt.xlabel('Confidence rating')
     plt.xticks(range(1, 8))
