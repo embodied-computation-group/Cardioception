dd8633592a123fe731aac74fafb23f3c4a74c42e
Nicolas Legrand <nicolas.legrand@cfin.au.dk>
2020-01-21 13:12:44 +0100

    Update detection oxi_peak function (copy from systole)

--- cardioception/detection.py
+++ cardioception/detection.py
@@ -3,52 +3,95 @@ import numpy as np
 from scipy.signal import find_peaks
 
 
-def oxi_peaks(x, sfreq=75, win=1):
-    """Peak detection on PPG recording.
+def oxi_peaks(x, sfreq=75, win=1, new_sfreq=1000, clipping=True,
+              noise_removal=True, peak_enhancement=True):
+    """A simple peak finder for PPG signal.
 
     Parameters
     ----------
-    x : list or Numpy array
+    x : list or 1d array-like
         The oxi signal.
-    sfreq = int
+    sfreq : int
         The sampling frequency. Default is set to 75 Hz.
     win : int
         Window size (in seconds) used to compute the threshold.
+    new_sfreq : int
+        If resample is *True*, the new sampling frequency.
+    resample : bool
+        If *True (defaults), will resample the signal at *new_sfreq*. Default
+        value is 1000 Hz.
 
-    Retruns
+    Returns
     -------
-    peaks : Numpy array
+    peaks : 1d array-like
         Numpy array containing R peak timing, in sfreq.
+    resampled_signal : 1d array-like
+        Signal resampled to the `new_sfreq` frequency.
 
     Notes
     -----
-    Signal squaring and detection of peaks using threshold set by the moving
-    averagte + standard deviation.
+    This algorithm use a simple rolling average to detect peaks. The signal is
+    first resampled and a rolling average is applyed to correct high frequency
+    noise and clipping. The signal is then squared and detection of peaks is
+    performed using threshold set by the moving averagte + stadard deviation.
+
+    .. warning :: This function will resample the signal to 1000 Hz.
+
+    References
+    ----------
+    Some of the processing steps were adapted from the HeartPy toolbox [1]:
+    https://python-heart-rate-analysis-toolkit.readthedocs.io/en/latest/index.html
+
+    [1] : van Gent, P., Farah, H., van Nes, N. and van Arem, B., 2019.
+    Analysing Noisy Driver Physiology Real-Time Using Off-the-Shelf Sensors:
+    Heart Rate Analysis Software from the Taking the Fast Lane Project. Journal
+    of Open Research Software, 7(1), p.32. DOI: http://doi.org/10.5334/jors.241
     """
     if isinstance(x, list):
         x = np.asarray(x)
 
-    # Square signal
-    x = x ** 2
+    # Interpolate
+    f = interpolate.interp1d(np.arange(0, len(x)/sfreq, 1/sfreq),
+                             x,
+                             fill_value="extrapolate")
+    time = np.arange(0, len(x)/sfreq, 1/new_sfreq)
+    x = f(time)
 
-    # Compute moving mean + standard deviation
-    win = int(win * sfreq)
-    rm = []
-    for i in range(len(x)):
+    # Copy resampled signal for output
+    resampled_signal = np.copy(x)
 
-        if i < win/2:
-            rm.append(np.mean(x[:win]) + np.std((x[:win])))
+    # Remove clipping artefacts with cubic interpolation
+    if clipping is True:
+        x = interpolate_clipping(x)
 
-        elif (i >= win/2) & (i < len(x - win)):
-            rm.append(np.mean(x[i-int(win/2):i+int(win/2)]) +
-                      np.std(x[i-int(win/2):i+int(win/2)]))
-        else:
-            rm.append(np.mean(x[-win:]) + np.std(x[-win:]))
+    if noise_removal is True:
+        # Moving average (high frequency noise + clipping)
+        rollingNoise = int(new_sfreq*.1)  # 0.1 second window
+        x = pd.DataFrame(
+            {'signal': x}).rolling(rollingNoise,
+                                   center=True).mean().signal.values
+    if peak_enhancement is True:
+        # Square signal (peak enhancement)
+        x = x ** 2
 
-    # Substract moving mean + standard deviation
-    x = x - rm
+    # Compute moving average and standard deviation
+    signal = pd.DataFrame({'signal': x})
+    mean_signal = signal.rolling(int(new_sfreq*0.75),
+                                 center=True).mean().signal.values
+    std_signal = signal.rolling(int(new_sfreq*0.75),
+                                center=True).std().signal.values
+
+    # Substract moving average + standard deviation
+    x -= (mean_signal + std_signal)
 
     # Find positive peaks
-    peaks = find_peaks(x, height=0)[0]
+    peaks_idx = find_peaks(x, height=0)[0]
 
-    return peaks
+    # Create boolean vector
+    peaks = np.zeros(len(x))
+    peaks[peaks_idx] = 1
+
+    if len(peaks) != len(x):
+        raise ValueError('Inconsistent output lenght')
+
+    return resampled_signal, peaks
