09cbc81c16c576c6f37a498e9872b0d215d8c19f
Nicolas Legrand <nicolas.legrand@cfin.au.dk>
2020-01-27 14:00:57 +0100

    Version 0.1.0

--- cardioception/__init__.py
+++ cardioception/__init__.py
@@ -3,4 +3,4 @@ from .detection import *
 from .HeartRateDiscrimination import *
 from .HeartBeatCounting import *
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ LONG_DESCRIPTION = """Measuring interoceptive performance with Psychopy.
 DISTNAME = 'cardioception'
 MAINTAINER = 'Nicolas Legrand'
 MAINTAINER_EMAIL = 'nicolas.legrand@cfin.au.dk'
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 
 INSTALL_REQUIRES = [
     'numpy>=1.15',
