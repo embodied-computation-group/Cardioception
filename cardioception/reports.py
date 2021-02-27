7f77f247e5ecbecd2fe70b17b8a4a29899708572
LegrandNico <nicolas.legrand@cfin.au.dk>
2021-02-27 01:01:54 +0100

    Add the reports submodule to automatically generate HTML reports from the result path.

---
+++ cardioception/reports.py
@@ -0,0 +1,42 @@
+# Author: Nicolas Legrand <nicolas.legrand@cfin.au.dk>
+
+import os
+import subprocess
+from typing import Optional
+
+import papermill as pm
+import pkg_resources
+
+
+def report(resultPath: str, reportPath: Optional[str] = None, task: str = "HRD"):
+    """Create HTML Report
+
+    Parameters
+    ----------
+    resultPath : str
+        Path variable. Where the results are stored (one participant only).
+    reportPath : Optional[str], optional
+        Where the HTML report should be saved. If `None`, default will be in
+        the provided `resultPath`.
+    task : str, optional
+        The task ("HRD" or "HBC"), by default "HRD".
+    """
+    temp_notebook = os.path.join(resultPath, "temp.ipynb")
+    htmlreport = os.path.join(resultPath, f"{task}_report.html")
+
+    if task == "HRD":
+        template = "HeartRateDiscrimination.ipynb"
+    elif task == "HBC":
+        template = "HeartBeatCounting.ipynb"
+
+    pm.execute_notebook(
+        pkg_resources.resource_filename("notebooks", template),
+        temp_notebook,
+        parameters=dict(resultPath=resultPath, reportPath=reportPath),
+    )
+    command = (
+        "jupyter nbconvert --to html --execute "
+        + f"--TemplateExporter.exclude_input=True {temp_notebook} --output {htmlreport}"
+    )
+    subprocess.call(command)
+    os.remove(temp_notebook)
