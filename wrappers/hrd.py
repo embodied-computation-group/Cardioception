4c2f2911623f229bc31a6b3d18d66eb3c77c0f07
LegrandNico <nicolas.legrand@cfin.au.dk>
2021-09-17 13:02:27 +0200

    HRD: better wrapper

--- wrappers/hrd.py
+++ wrappers/hrd.py
@@ -6,20 +6,28 @@ from psychopy import gui
 from cardioception.HRD.parameters import getParameters
 from cardioception.HRD.task import run
 
+################################
+# Heart rate Discrimination task
+################################
+
 # Create a GUI and ask for high-evel experiment parameters
 g = gui.Dlg()
 g.addField("participant", initial="Participant")
 g.addField("session", initial="HRD")
-g.addField("Serial Port:", initial=None)
-g.addField("Setup:", initial="behavioral")
+g.addField("Serial Port:", initial="COM5")
+g.addField("Setup:", initial="behavioral", choices=["behavioral", "test", "fMRI"])
+g.addField("Device:", initial="mouse", choices=["mouse", "keyboard"])
+g.addField("Language:", initial="english", choices=["english", "danish"])
 g.show()
 
-# Set global task parameters here
+# Set global task parameters herecd
 parameters = getParameters(
     participant=g.data[0],
     session=g.data[1],
     serialPort=g.data[2],
     setup=g.data[3],
+    device=g.data[4],
+    language=g.data[5],
     stairType="psi",
     catchTrials=0.0,
     nTrials=120,
