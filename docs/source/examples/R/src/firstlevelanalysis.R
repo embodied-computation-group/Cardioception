ce60ab2c536eff7f7101d274c74b87d9620ff28a
JesperFischer <jesperfischer@outlook.dk>
2023-10-06 12:20:06 +0200

    bug_fix

--- docs/source/examples/R/src/firstlevelanalysis.R
+++ docs/source/examples/R/src/firstlevelanalysis.R
@@ -9,10 +9,10 @@ source(here("docs", "source", "examples", "R", "src", "helpers.R"))
 # the main function to run a single person analysis
 single_sub_analysis <- function(df, interoPost = NA, exteroPost = NA, bayesian = FALSE, model = NA, out) {
   # starting off finding the columns of interrest and making sure they are the right format!
-
+  
   df$ResponseCorrect <- ifelse(df$ResponseCorrect == "", NA, df$ResponseCorrect)
   df$Decision <- ifelse(df$Decision == "", NA, df$Decision)
-
+  
   df <- df %>%
     mutate(
       Decision = as.character(df$Decision),
@@ -26,77 +26,84 @@ single_sub_analysis <- function(df, interoPost = NA, exteroPost = NA, bayesian =
       EstimatedThreshold = as.numeric(df$EstimatedThreshold),
       EstimatedSlope = as.numeric(df$EstimatedSlope),
     )
-
+  
   # check for NA's in the critical columns of the data:
   # specify different ways of coding missing values:
   missing_pres <- c(NA, "na", "N/A", "NaN", NaN, "n/a")
-
+  
   trials_missing <- df %>%
     select(Decision, Confidence, ConfidenceRT, Confidence, Condition, listenBPM, responseBPM, nTrials) %>%
     filter_all(any_vars(. %in% missing_pres)) %>%
     .$nTrials
-
-
-
+  
+  
+  
   if (length(trials_missing) != 0) {
     print(paste("Number of NA's = ", length(trials_missing), " detected in trials : "))
     print(as.character(trials_missing))
   }
   # remove the NA's
   df1 <- df %>% filter(!nTrials %in% trials_missing)
-
-
-
+  
+  
+  
   # find number of modalities:
   n_mod <- length(unique(df1$Modality))
-
+  
   if (n_mod > 2) {
     print("More than 2 modalities are not supported (yet)")
   }
   if (is.null(df$id[1])) {
     # give the resulting dataframe (main subject results a random id)
-    id <- runif(1, min = 1, max = 1000)
+    id <- round(runif(1, min = 1, max = 10000),0)
     # make the data frame:
     resultsdata <- data.frame(ids = rep(id, n_mod))
   } else {
     resultsdata <- data.frame(ids = rep(df$id[1], n_mod))
   }
+  
+  output_dir = paste0(out,"/results_sub",resultsdata$id[1])
+  #create an index for if the directory exists such that we dont overwrite files:
+  idx = round(runif(1,min = 1, max = 1000),0)
+  
   # create a folder to put the results:
-  dir.create(paste0(out,"/results_sub"))
-
+  if(!dir.exists(output_dir)){
+    dir.create(output_dir)
+    idx = ""
+  }
   # getting the plots from the help function
-
+  
   reactiontimeplot <- reaction_time_plot(df, 3, 0.4, n_mod)
   stat <- reactiontimeplot[[2]]
-
+  
   # append the main statistics from the reaction time plot to the resulting dataframe
   resultsdata <- cbind(resultsdata, stat)
-
+  
   # get the reaction time plot where we exclude the legends.
   reactiontimeplot <- reactiontimeplot[[1]] + guides(fill = "none")
-
+  
   # get the citerion and d' and the table:
   stat <- sum_stat(df)
-
+  
   # append results
   resultsdata <- cbind(resultsdata, stat[[2]])
-
+  
   # use the dataframe with removed NA's
   # get confidence histogram plot
   confidenceplot <- plot_confidence(df1, n_mod)
-
+  
   # get mean accuracy
   results <- get_mean_acc(df)
-
+  
   # append results
   resultsdata <- cbind(resultsdata, results)
-
-
-
+  
+  
+  
   # get the intensityplot and remove legends and title
   intensityplot <- intensity_plot(df)
   intensityplot <- intensityplot + guides(fill = "none") + ggtitle(" ")
-
+  
   # get interval plot:
   if (is.na(exteroPost[1]) == TRUE && is.na(interoPost[1]) == TRUE) {
     intervalplot <- ggplot() +
@@ -111,47 +118,47 @@ single_sub_analysis <- function(df, interoPost = NA, exteroPost = NA, bayesian =
   if (is.na(exteroPost[1]) == FALSE && is.na(interoPost[1]) == FALSE) {
     intervalplot <- plot_interval(df1, exteroPost, interoPost)
   }
-
+  
   # get analysis plot (alpha vs probability of answering more)
   analysisplot <- analysis_plot(df)
   stats <- analysisplot[[2]]
-
+  
   # append results of this plot
   resultsdata <- cbind(resultsdata, stats)
-
+  
   analysisplot <- analysisplot[[1]] + guides(fill = "none", linetype = "none", color = "none")
-
+  
   # make the composit-plot with patchwork
   plot <- (reactiontimeplot + confidenceplot) / (analysisplot + intensityplot + intervalplot) + plot_layout(nrow = 2)
-
+  
   # save the plot:
-  ggsave(paste0(out,"/results_sub/resultplot_basic.png"), plot, width = 4000, height = 2200, units = "px")
-
-
+  ggsave(paste0(output_dir,"/resultplot_basic",idx,".png"), plot, width = 4000, height = 2200, units = "px")
+  
+  
   # if the bayesian analysis is selected:
   if (bayesian == TRUE) {
     if (n_mod == 2) {
       # run bayesian analysis on Extero and Intero and append the statistics to the dataframe (resultsdata)
       baysextero <- baysiananalysis(df, "Extero", model)
       stats <- baysextero[[4]]
-
+      
       baysintero <- baysiananalysis(df, "Intero", model)
       stats <- rbind(stats, baysintero[[4]])
-
+      
       resultsdata <- cbind(resultsdata, stats)
-
+      
       baysplot_ex <- baysextero[[1]] + baysextero[[2]] + baysextero[[3]] + plot_layout(design = c(
         patchwork::area(1, 1, 1, 1),
         patchwork::area(1, 2, 1, 2),
         patchwork::area(2, 1, 3, 2)
       ))
-
+      
       baysplot_in <- baysintero[[1]] + baysintero[[2]] + baysintero[[3]] + plot_layout(design = c(
         patchwork::area(1, 1, 1, 1),
         patchwork::area(1, 2, 1, 2),
         patchwork::area(2, 1, 3, 2)
       ))
-
+      
       baysplot <- baysextero[[1]] + baysextero[[2]] + baysextero[[3]] + baysintero[[1]] + baysintero[[2]] + baysintero[[3]] + plot_layout(design = c(
         patchwork::area(1, 1, 1, 1),
         patchwork::area(1, 2, 1, 2),
@@ -160,36 +167,36 @@ single_sub_analysis <- function(df, interoPost = NA, exteroPost = NA, bayesian =
         patchwork::area(1, 4, 1, 4),
         patchwork::area(2, 3, 3, 4)
       ))
-
+      
       # save the figures
-      ggsave(paste0(out,"/results_sub/resultplot_bayse_intero.png"), baysplot_in, width = 4000, height = 2200, units = "px")
-      ggsave(paste0(out,"/results_sub/resultplot_bayse_extero.png"), baysplot_ex, width = 4000, height = 2200, units = "px")
-      ggsave(paste0(out,"/results_sub/resultplot_bayse.png"), baysplot, width = 4000, height = 2200, units = "px")
-
+      ggsave(paste0(output_dir,"/resultplot_bayse_intero",idx,".png"), baysplot_in, width = 4000, height = 2200, units = "px")
+      ggsave(paste0(output_dir,"/resultplot_bayse_extero",idx,".png"), baysplot_ex, width = 4000, height = 2200, units = "px")
+      ggsave(paste0(output_dir,"/resultplot_bayse",idx,".png"), baysplot, width = 4000, height = 2200, units = "px")
+      
       baysplot <- list(baysplot_ex, baysplot_in, baysplot)
     }
-
+    
     if (n_mod == 1) {
       bayse <- baysiananalysis(df, as.character(unique(df$Modality)), model)
       stats <- bayse[[4]]
       resultsdata <- cbind(resultsdata, stats)
-
+      
       baysplot <- bayse[[1]] + bayse[[2]] + bayse[[3]] + plot_layout(design = c(
         patchwork::area(1, 1, 1, 1),
         patchwork::area(1, 2, 1, 2),
         patchwork::area(2, 1, 3, 2)
       ))
-      ggsave(paste0(out,"/results_sub/resultplot_bayse.png"), baysplot, width = 4000, height = 2200, units = "px")
+      ggsave(paste0(output_dir,"/resultplot_bayse",idx,".png"), baysplot, width = 4000, height = 2200, units = "px")
     }
-
+    
     # delete all duplicate columns in the resulting dataframe
-
+    
     resultsdata <- resultsdata[, !duplicated(colnames(resultsdata))]
     # give it sensisble rownames:
     rownames(resultsdata) <- 1:nrow(resultsdata)
     # save it
-    write.csv(resultsdata, paste0(out,"/results_sub/data.csv"))
-
+    write.csv(resultsdata, paste0(output_dir,"/data",idx,".csv"))
+    
     return(list(rt_plot = reactiontimeplot, summary_stat = stat, conf_plot = confidenceplot, staircase_plot = intervalplot, histogram_plot = intensityplot, analysis_plot = analysisplot, concatenated_plot = plot, stats = resultsdata, bayesian_plot = baysplot))
   }
   # delete all duplicate columns in the resulting dataframe
@@ -197,8 +204,8 @@ single_sub_analysis <- function(df, interoPost = NA, exteroPost = NA, bayesian =
   # give it sensisble rownames:
   rownames(resultsdata) <- 1:nrow(resultsdata)
   # save it
-  write.csv(resultsdata, paste0(out,"/results_sub/data.csv"))
-
+  write.csv(resultsdata, paste0(output_dir,"/data",idx,".csv"))
+  
   return(list(rt_plot = reactiontimeplot, summary_stat = stat, conf_plot = confidenceplot, staircase_plot = intervalplot, histogram_plot = intensityplot, analysis_plot = analysisplot, concatenated_plot = plot, stats = resultsdata))
 }
 
@@ -210,53 +217,65 @@ study_analysis <- function(path, bayesian, model, folder = T, out) {
   if (folder) {
     sub_folders <- list.dirs(path = path, full.names = TRUE, recursive = FALSE)
     print(paste0("found ", length(sub_folders), " directories"))
-
+    
     combined_data <- data.frame()
     for (i in 1:length(sub_folders)) {
-      setwd(sub_folders[i])
-
-      filelist <- list.files(pattern = "*.txt")
-
+      
+      filelist <- list.files(path =  paste0(sub_folders[i]), pattern = "*_final.txt")
+      
       if (!is.character(filelist)) {
-        print("To many text files")
+        print("To many text files with _final.txt")
       }
-
-      data <- read_csv(filelist[1])
-
-      numpy_filelist <- list.files(pattern = "*.npy")
-
+      
+      data <- read_csv(paste0(sub_folders[i],"/",filelist[1]))
+      
+      numpy_filelist <- list.files(path =  paste0(sub_folders[i]),pattern = "*.npy")
+      
       if (length(numpy_filelist) != 2) {
         print("Not 2 numpy files")
       }
-
-      exteroPost <- np$load(numpy_filelist[str_detect(tolower(numpy_filelist), "extero")])
-      interoPost <- np$load(numpy_filelist[str_detect(tolower(numpy_filelist), "intero")])
-
-
+      
+      numpy_filelist = paste0(sub_folders[i],"/",numpy_filelist)
+      
+      
+      
+      tryCatch(
+        {
+          exteroPost <- np$load(numpy_filelist[str_detect(tolower(numpy_filelist), "extero")])
+          interoPost <- np$load(numpy_filelist[str_detect(tolower(numpy_filelist), "intero")])
+          
+        },
+        error = function(err) {
+          exteroPost <<- NA
+          interoPost <<- NA
+          print("No numpy files found")
+        }
+      )
+      
+      
       liste <- single_sub_analysis(data, interoPost = interoPost, exteroPost = exteroPost, bayesian = bayesian, model = model, out = out)
-
+      
       combined_data <- rbind(combined_data, liste$stats)
     }
-
+    
     write.csv(combined_data, paste0(out,"/resulting_dataframe.csv"))
   } else {
-    sub_files <- list.files(path = path, full.names = TRUE, recursive = FALSE, pattern = "*.txt")
-    print(paste0("found ", length(sub_files), " files"))
-
+    sub_files <- list.files(path = path, full.names = TRUE, recursive = FALSE, pattern = "*final.txt")
+    print(paste0("found ", length(sub_files), "files"))
+    
     combined_data <- data.frame()
     for (i in 1:length(sub_files)) {
       print(i)
-
-
+      
+      
       if (!is.character(sub_files[i])) {
         print("To many text files")
       }
-
+      
       data <- read_csv(sub_files[i])
-
+      
       numpy_filelist <- list.files(pattern = "*.npy")
-
-
+      
       tryCatch(
         {
           exteroPost <- np$load(numpy_filelist[str_detect(tolower(numpy_filelist), "extero")])
@@ -268,15 +287,16 @@ study_analysis <- function(path, bayesian, model, folder = T, out) {
           print("No numpy files found")
         }
       )
-
-
-
+      
+      
+      
       liste <- single_sub_analysis(data, interoPost = interoPost, exteroPost = exteroPost, bayesian = bayesian, model = model, out = out)
-
+      
       combined_data <- rbind(combined_data, liste$stats)
+      write.csv(combined_data, paste0(out,"/resulting_dataframe.csv"))
+      
     }
-
-    write.csv(combined_data, paste0(out,"/resulting_dataframe.csv"))
+    
   }
-  return(combined_data)
+  
 }
